b8983dba2a77ada68420a9fe17ab33423ba6ce29
alexiadb <49482351+alexiadb@users.noreply.github.com>
2022-01-21 17:01:31 +0100

    final push with dashboard

--- script brouillon.py
+++ script brouillon.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan  7 18:14:22 2022
 
-@author: alexiadeboynes
-"""
 
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
@@ -35,13 +29,4 @@ engine = create_engine(str, echo=False)
 str
 
 #ecriture du df ds la bdd
-
-customers = pd.read_csv('olist_customers_dataset.csv', sep= ',', decimal= '.', )
-print(customers)
-
-customers.to_sql('customers', con=engine, if_exists='append', index=False)
-'''
-orderItems = pd.read_csv('olist_customers_dataset.csv', index_col='customer_id', sep= ',', decimal= '.', )
-print(customers)
-
-orderItems.to_sql('orderItems', con=engine, if_exists='replace', index=False)'''
+path = '/Users/alexiadeboynes/Documents/data/g4brazilian/'
--- script.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan  7 12:59:25 2022
-
-@author: alexiadeboynes
-"""
-
